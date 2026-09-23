9e02fb9186b0834fc7ed8303cd8049abf777af00
Petko Georgiev <petko0362@gmail.com>
2026-09-23 16:17:56 +0300

    Добавен конструктор

--- StudentApp/Student.cs
+++ StudentApp/Student.cs
@@ -10,5 +10,12 @@ namespace StudentApp
     {
         public string Name { get; set; }
         public int Age { get; set; }
+
+        public Student(string name, int age)
+
+        {
+            Name = name;
+            Age = age;
+        }
     }
 }
